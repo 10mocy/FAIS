fb28789d9ac6bba1f8d73107140ebc9a411d7599
Tomotada Saigusa <nirot1r@g-second.net>
2020-02-06 03:23:22 +0900

    #16 [v1.32-beta] 登録ログ機能実装

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
 import Discord from 'discord.js';
+import moment from 'moment';
+
+import * as Shovel from './helpers/shovel-db';
 
 class FAISBot {
-  private token!: string;
-  private watchChannelId!: string;
+  private token: string;
+  private watchChannelId: string;
 
   private bot: Discord.Client;
+  private shovel: Shovel.DB;
 
   constructor(token: string | undefined, chId: string | undefined) {
     if (!token) {
@@ -18,12 +22,14 @@ class FAISBot {
 
     this.token = token;
     this.bot = new Discord.Client();
+    this.shovel = new Shovel.DB('mongodb://localhost:27017', 'fais');
 
     this.watchChannelId = chId;
   }
 
-  public start(): void {
+  public async start(): Promise<void> {
     this.bot.login(this.token);
+    this.shovel.start();
 
     this.handleReady();
     this.handleError();
@@ -50,7 +56,11 @@ class FAISBot {
 
       this.handleChat(msg);
       this.handleInviteLink(msg);
-      this.handleCommand(msg);
+      this.handleInviteCommand(msg);
+
+      this.handleShovelCommand(msg);
+
+      this.handleSystemCommand(msg);
     });
   }
 
@@ -88,13 +98,108 @@ class FAISBot {
       .catch(err => console.error(err));
   }
 
-  private handleCommand(msg: Discord.Message): void {
+  private handleInviteCommand(msg: Discord.Message): void {
     const inviteLinkCmd = msg.content.match(/\/invite (.+)/);
     if (inviteLinkCmd) {
       this.deleteMessage(msg);
       msg.channel
         .send(`https://discord.gg/${inviteLinkCmd[1]}`)
         .catch(err => console.error(err));
+    }
+  }
+
+  private handleSystemCommand(msg: Discord.Message): void {
+    const systemCmd = msg.content.match(/^!fais (\S+)(.*)$/);
+    if (!systemCmd) return;
+
+    const args = systemCmd[2].replace(/\s+/g, ' ').split(' ');
+    args.shift();
+
+    if (systemCmd[1] === 'findWord') {
+      if (args.length !== 1) {
+        msg.channel.send('引数が不正です / 使用例 : `!fais findWord <単語>`');
+        return;
+      }
+
+      this.shovel
+        .findWord({ word: args[0] })
+        .then((word: Shovel.Word | undefined) => {
+          if (word) {
+            msg.channel.send({
+              embed: {
+                title: `「${args[0]}」の照会結果`,
+                color: parseInt('0x53eb34', 16),
+                fields: [
+                  {
+                    name: 'よみ',
+                    value: word.yomi || '(読まない)',
+                    inline: false
+                  },
+                  {
+                    name: '単語登録者',
+                    value: `${word.userTag}`,
+                    inline: true
+                  },
+                  {
+                    name: '登録日',
+                    value: `${moment
+                      .unix(word.timestamp ? word.timestamp : 0)
+                      .format('YYYY/MM/DD HH:mm:ss')}`,
+                    inline: true
+                  }
+                ]
+              }
+            });
+          } else {
+            msg.channel.send({
+              embed: {
+                title: `${args[0]}の照会結果`,
+                color: parseInt('0xeb4034', 16),
+                description: `「${args[0]}」は辞書に登録されていません`
+              }
+            });
+          }
+        });
+    }
+  }
+
+  private handleShovelCommand(msg: Discord.Message): void {
+    const shovelCmd = msg.content.match(
+      /^!sh[gr]? (?:(add|delete)_word|([ad])w) (\S+)(?: (\S+))?$/
+    );
+    if (!shovelCmd) return;
+
+    if (shovelCmd[1] == 'add' || shovelCmd[2] == 'a') {
+      msg.react('📝');
+
+      this.shovel
+        .addWord({
+          word: shovelCmd[3],
+          yomi: shovelCmd[4],
+          userTag: msg.author.tag,
+          userId: msg.author.id
+        })
+        .then(() => {
+          console.log(`success 単語データ登録完了 ${shovelCmd[3]}`);
+          msg.react('✅');
+        })
+        .catch(err => {
+          console.error(`error ${err}`);
+          msg.react('❎');
+        });
+    } else if (shovelCmd[1] == 'delete' || shovelCmd[2] == 'd') {
+      msg.react('🗑️');
+
+      this.shovel
+        .removeWord({ word: shovelCmd[3] })
+        .then(() => {
+          console.log(`success 単語データ削除完了 ${shovelCmd[3]}`);
+          msg.react('✅');
+        })
+        .catch(err => {
+          console.error(`error ${err}`);
+          msg.react('❎');
+        });
     }
   }
 
