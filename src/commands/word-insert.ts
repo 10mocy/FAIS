2c8d7de22f2ff6369ae23302f8bf05ff50e55757
Tomotada Saigusa <nirot1r@g-second.net>
2020-02-06 20:33:45 +0900

    #16 単語追加コマンド実装(管理用)

---
+++ src/commands/word-insert.ts
@@ -0,0 +1,28 @@
+import * as Shovel from './../helpers/shovel-db';
+
+const main = async (): Promise<void> => {
+  if (process.argv.length !== 7) {
+    console.error('引数が足りません');
+    return process.exit(-1);
+  }
+
+  const db = new Shovel.DB('mongodb://localhost:27017', 'fais');
+  await db.start();
+
+  db.addWord({
+    word: process.argv[2],
+    yomi: process.argv[3],
+    userTag: process.argv[4],
+    userId: process.argv[5],
+    messageUri: process.argv[6]
+  })
+    .then(() => {
+      console.log(`success 単語データ登録完了 ${process.argv[2]}`);
+      process.exit(0);
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+main();
